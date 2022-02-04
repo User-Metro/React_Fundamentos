e1f9ec6dae0ad812ee00730f8371c7fa4c879fb2
Abdon <estadia2020@gmail.com>
2022-02-04 16:06:23 -0600

    hola mundo vanilla js

--- src/temario/hello.js
+++ src/temario/hello.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 export default class Hello extends Component {
 
 	render(){
+	const HolaMundo = '¡Hola mundo!';
+	const rootApp = document.getElementById('root');
+
+	rootApp.innerHTML = `<h2>Este es mi primer, ${HolaMundo} con vanilla js </h2>`
+
 		return (
-      <h1>Mi primer hola mundo con React</h1>
+      <div id="root"></div>
 		)
 	}
 }
